e5718bbb933162aba310778a6ae0a56c3d67c122
Julian Kahnert <mail@juliankahnert.de>
2020-01-17 23:11:26 +0100

    license/readme content check added

--- Sources/chiaLib/Internal/CheckProviders/LicenseCheck.swift
+++ Sources/chiaLib/Internal/CheckProviders/LicenseCheck.swift
@@ -12,9 +12,17 @@ struct LicenseCheck: CheckProvider {
     static let languages: [Language] = [.generic]
     static let dependencies: [String] = []
     private static let missingFileResult = [CheckResult(severity: .error, message: "LICENSE file could not be found.", metadata: nil)]
+    private static let missingContentResult = [CheckResult(severity: .error, message: "No content in LICENSE file found.", metadata: nil)]
 
     static func run(with config: ChiaConfig, at projectRoot: Folder) throws -> [CheckResult] {
-        let fileMissing = !projectRoot.containsFile(at: "LICENSE")
-        return fileMissing ? missingFileResult : []
+
+        // try to find file
+        guard let file = try? projectRoot.file(at: "LICENSE") else { return missingFileResult }
+
+        // try to get the file content
+        guard let content = try? String(contentsOf: file.url),
+            !content.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else { return missingContentResult }
+
+        return []
     }
 }
--- Sources/chiaLib/Internal/CheckProviders/ReadmeCheck.swift
+++ Sources/chiaLib/Internal/CheckProviders/ReadmeCheck.swift
@@ -12,9 +12,17 @@ struct ReadmeCheck: CheckProvider {
     static let languages: [Language] = [.generic]
     static let dependencies: [String] = []
     private static let missingFileResult = [CheckResult(severity: .error, message: "README.md file could not be found.", metadata: nil)]
+    private static let missingContentResult = [CheckResult(severity: .error, message: "No content in README.md file found.", metadata: nil)]
 
     static func run(with config: ChiaConfig, at projectRoot: Folder) throws -> [CheckResult] {
-        let fileMissing = !projectRoot.containsFile(at: "README.md")
-        return fileMissing ? missingFileResult : []
+
+        // try to find file
+        guard let file = try? projectRoot.file(at: "README.md") else { return missingFileResult }
+
+        // try to get the file content
+        guard let content = try? String(contentsOf: file.url),
+            !content.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else { return missingContentResult }
+
+        return []
     }
 }
